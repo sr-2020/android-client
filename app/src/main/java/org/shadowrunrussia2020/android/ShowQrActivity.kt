9fa4ae2824dfa92228fb1efc7f10cb4a835b322b
Alexey Eremin <a.eremin.msu@gmail.com>
2019-05-30 16:39:18 +0200

    Cleanups

--- app/src/main/java/org/shadowrunrussia2020/android/ShowQrActivity.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/ShowQrActivity.kt
@@ -1,17 +1,15 @@
 package org.shadowrunrussia2020.android
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.navigation.navArgs
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import kotlinx.android.synthetic.main.activity_show_qr.*
-import org.shadowrunrussia2020.android.qr.Data
 import org.shadowrunrussia2020.android.qr.encode
 
 class ShowQrActivity : Activity() {
-    val args: ShowQrActivityArgs by navArgs()
+    private val args: ShowQrActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_qr)
