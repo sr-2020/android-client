bd876c9522cb7de0ca3e80012f2faf1f52339114
Alexey Eremin <a.eremin.msu@gmail.com>
2019-06-01 12:32:42 +0200

    Trigger transfer by QR

--- app/src/main/java/org/shadowrunrussia2020/android/qr/BillScannedFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/qr/BillScannedFragment.kt
@@ -1,15 +1,28 @@
 package org.shadowrunrussia2020.android.qr
 
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.transition.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_bill_scanned.*
+import kotlinx.android.synthetic.main.recycler_view_item.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.shadowrunrussia2020.android.R
+import org.shadowrunrussia2020.android.billing.BillingViewModel
 
 
 class BillScannedFragment : Fragment() {
@@ -22,15 +35,28 @@ class BillScannedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Toast.makeText(
-            activity!!,
-            "Пытаемся перевести %d нью-йен на аккаунт %d, комментарий: %s".format(
-                args.transfer.amount,
-                args.transfer.sin_to,
-                args.transfer.comment
-            ),
-            Toast.LENGTH_LONG
-        ).show()
-        findNavController().navigate(R.id.action_global_back_to_main)
+        progressLoader.visibility = View.INVISIBLE
+        val t = args.transfer
+        AlertDialog.Builder(activity!!)
+            .setMessage(
+                "Запрос о переводе %d нью-йен на аккаунт %d. Комментарий: %s. Подтверждаете перевод?".format(
+                    t.amount,
+                    t.sin_to,
+                    t.comment
+                )
+            )
+            .setPositiveButton(R.string.ok) { _, _ ->
+                CoroutineScope(Dispatchers.Main).launch {
+                    val m = ViewModelProviders.of(activity!!).get(BillingViewModel::class.java)
+                    progressLoader.visibility = View.VISIBLE
+                    withContext(CoroutineScope(Dispatchers.IO).coroutineContext)
+                    { m.transferMoney(receiver = t.sin_to, amount = t.amount, comment = t.comment) }
+                    findNavController().navigate(R.id.action_global_back_to_main)
+                }
+            }
+            .setNegativeButton(R.string.cancel) { _, _ ->
+                findNavController().navigate(R.id.action_global_back_to_main)
+            }
+            .create().show()
     }
 }
