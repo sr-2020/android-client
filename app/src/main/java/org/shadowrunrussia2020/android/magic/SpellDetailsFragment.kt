337e4ddc2acbc2e5e0dea19e8335dc41dbbe10a0
Alexey Eremin <a.eremin.msu@gmail.com>
2019-09-01 18:43:57 +0200

    Allow to choose spell power

--- app/src/main/java/org/shadowrunrussia2020/android/magic/SpellDetailsFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/magic/SpellDetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -16,15 +17,22 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.shadowrunrussia2020.android.R
 import org.shadowrunrussia2020.android.character.CharacterViewModel
+import org.shadowrunrussia2020.android.character.models.Character
 import org.shadowrunrussia2020.android.character.models.Spell
 import org.shadowrunrussia2020.android.qr.*
 
 class SpellDetailsFragment : Fragment() {
     private val args: SpellDetailsFragmentArgs by navArgs()
     private val spell: Spell by lazy { args.spell }
-    private val mModel by lazy { ViewModelProviders.of(requireActivity()).get(CharacterViewModel::class.java) }
+    private val mModel by lazy {
+        ViewModelProviders.of(requireActivity()).get(CharacterViewModel::class.java)
+    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_spell_details, container, false)
     }
 
@@ -46,12 +54,28 @@ class SpellDetailsFragment : Fragment() {
 
         castOnSelf.setOnClickListener { castOnSelf() }
         castOnTarget.setOnClickListener { chooseTarget() }
+
+        mModel.getCharacter()
+            .observe(this, Observer { data: Character? ->
+                if (data != null) {
+                    seekBarSpellPower.max = data.magic + data.magicPowerBonus
+                    val canCast = seekBarSpellPower.max > 0
+                    castOnSelf.isEnabled = spell.canTargetSelf && canCast
+                    castOnTarget.isEnabled =
+                        (spell.canTargetItem || spell.canTargetSingleTarget) && canCast
+                    castOnLocation.isEnabled = (spell.canTargetLocation) && canCast
+                }
+            })
     }
 
     private fun castOnSelf() {
         CoroutineScope(Dispatchers.Main).launch {
             try {
-                withContext(CoroutineScope(Dispatchers.IO).coroutineContext) { mModel.postEvent(spell.eventType) }
+                withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
+                    mModel.postEvent(
+                        spell.eventType, hashMapOf("power" to seekBarSpellPower.progress)
+                    )
+                }
             } catch (e: Exception) {
                 showErrorMessage(requireContext(), "Ошибка. ${e.message}")
             }
@@ -64,9 +88,17 @@ class SpellDetailsFragment : Fragment() {
 
     private fun castOnTarget(qrData: Data) {
         val eventData = when {
-            qrData.type == Type.REWRITABLE -> hashMapOf("qrCode" to qrData.payload.toInt())
-            qrData.type == Type.DIGITAL_SIGNATURE || qrData.type == Type.WOUNDED_BODY -> hashMapOf("targetCharacterId" to qrData.payload.toInt())
-            else -> {showErrorMessage(requireContext(), "Ошибка. Неожиданный QR-код."); return}
+            qrData.type == Type.REWRITABLE -> hashMapOf(
+                "qrCode" to qrData.payload.toInt(),
+                "power" to seekBarSpellPower.progress
+            )
+            qrData.type == Type.DIGITAL_SIGNATURE || qrData.type == Type.WOUNDED_BODY -> hashMapOf(
+                "targetCharacterId" to qrData.payload.toInt(),
+                "power" to seekBarSpellPower.progress
+            )
+            else -> {
+                showErrorMessage(requireContext(), "Ошибка. Неожиданный QR-код."); return
+            }
         }
         CoroutineScope(Dispatchers.Main).launch {
             try {
