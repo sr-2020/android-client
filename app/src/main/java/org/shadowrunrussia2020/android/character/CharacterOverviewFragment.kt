0f4abec1b43324554bd25fe872c02ad5015c390c
Alexey Eremin <a.eremin.msu@gmail.com>
2019-09-03 19:06:01 +0200

    Remove unused code

--- app/src/main/java/org/shadowrunrussia2020/android/character/CharacterOverviewFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/character/CharacterOverviewFragment.kt
@@ -2,14 +2,13 @@ package org.shadowrunrussia2020.android.character
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.firebase.database.*
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_character_overview.*
 import kotlinx.coroutines.CoroutineScope
@@ -40,55 +39,6 @@ class CharacterOverviewFragment : Fragment() {
                 textViewMagicPowerBonus.text = it.magicPowerBonus.toString()
             }
         })
-
-        database.getReference("spellsCasted").addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val p = dataSnapshot.getValue(Int::class.java) ?: return
-                textTest1.text = p.toString()
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.w("Firebase", "loadPost:onCancelled", databaseError.toException())
-            }
-        })
-
-        buttonTest1.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                database.getReference("spellsCasted").runTransaction(object : Transaction.Handler {
-                    override fun doTransaction(mutableData: MutableData): Transaction.Result {
-                        val p = mutableData.getValue(Int::class.java)
-                            ?: return Transaction.success(mutableData)
-                        mutableData.value = p + 1
-                        return Transaction.success(mutableData)
-                    }
-
-                    override fun onComplete(p0: DatabaseError?, p1: Boolean, p2: DataSnapshot?) {}
-                })
-            }
-        }
-
-        val docRef = firestore.collection("characters").document("8")
-        docRef.addSnapshotListener { snapshot, e ->
-            if (e != null) {
-                Log.w("Firestore", "Listen failed.", e)
-                return@addSnapshotListener
-            }
-
-            if (snapshot != null && snapshot.exists()) {
-                Log.d("Firestore", "Current data: ${snapshot.data}")
-                textTest2.text = snapshot.data!!["spellsCasted"].toString()
-            } else {
-                Log.d("Firestore", "Current data: null")
-            }
-        }
-
-        buttonTest2.setOnClickListener {
-            firestore.runTransaction { transaction ->
-                val snapshot = transaction.get(docRef)
-                val newPopulation = snapshot.getLong("spellsCasted")!! + 1
-                transaction.update(docRef, "spellsCasted", newPopulation)
-            }
-        }
 
         swipeRefreshLayout.setOnRefreshListener { refreshData() }
     }
