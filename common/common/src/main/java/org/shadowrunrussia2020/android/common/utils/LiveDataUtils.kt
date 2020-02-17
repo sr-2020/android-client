547ae6ebe8dd7ece83114da159aa7817cda906b8
da.subbotenko <da.subbotenko@tensor.ru>
2020-02-17 15:13:10 +0300

    LifeData extentions

---
+++ common/common/src/main/java/org/shadowrunrussia2020/android/common/utils/LiveDataUtils.kt
@@ -0,0 +1,15 @@
+package org.shadowrunrussia2020.android.common.utils
+
+import androidx.annotation.MainThread
+import androidx.arch.core.util.Function
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+
+@MainThread
+fun <X, Y> LiveData<X>.map(mapFunction: Function<X?, Y>): LiveData<Y> = Transformations.map(this, mapFunction)
+
+@MainThread
+fun <X, Y> LiveData<X>.switchMap(switchMapFunction: Function<X?, LiveData<Y>>): LiveData<Y?> = Transformations.switchMap(this, switchMapFunction)
+
+@MainThread
+fun <X> LiveData<X>.distinctUntilChanged(): LiveData<X?> = Transformations.distinctUntilChanged(this)
