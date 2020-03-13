8420cc88bfee0c544f510f0fae6e871689dfcdcb
Alexey Eremin <a.eremin.msu@gmail.com>
2020-03-13 20:46:55 +0100

    Remove pile of duplicated unused files

--- model/scanners/src/main/java/org/shadowrunrussia2020/android/model/di/Dependency.kt
+++
@@ -1,8 +0,0 @@
-package org.shadowrunrussia2020.android.model.di
-
-import org.shadowrunrussia2020.android.common.di.ApplicationSingletonScope
-import org.shadowrunrussia2020.android.common.di.MainActivityScope
-
-//interface ActivityScopeModelDependency : MainActivityScope
-
-interface ModelDependency: ApplicationSingletonScope.Dependency
