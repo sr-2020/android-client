52ada3a3821dbc920d0676687d083a29f9470d81
Alexey Eremin <a.eremin.msu@gmail.com>
2020-03-29 19:51:22 +0200

    Show cooldown in activity lists

--- features/view/universal_list/src/main/java/org/shadowrunrussia2020/android/view/universal_list/ActiveAbilityListItem.kt
+++ features/view/universal_list/src/main/java/org/shadowrunrussia2020/android/view/universal_list/ActiveAbilityListItem.kt
@@ -4,11 +4,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.generic_recycler_view_item.view.*
 import org.ocpsoft.prettytime.PrettyTime
 import org.shadowrunrussia2020.android.common.models.ActiveAbility
+import org.shadowrunrussia2020.android.common.utils.MainThreadSchedulers
+import org.shadowrunrussia2020.android.common.utils.plusAssign
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 fun createActiveAbilityHeader(list:Collection<UniversalViewData>) = HeaderItem(R.id.active_ability_item, "Активируемые способности", R.drawable.statement, list)
 
@@ -23,20 +28,35 @@ class ActiveAbilityListItem(
 }
 
 private class ActiveAbilityViewHolder private constructor(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    private val disposer = CompositeDisposable()
+
     constructor(parent: ViewGroup)
             : this(LayoutInflater.from(parent.context).inflate(R.layout.generic_recycler_view_item, parent, false))
 
     fun bindView(activeAbility: ActiveAbility, onClick: (() -> Unit)?, hide: Boolean) {
+        disposer.clear()
+
         containerView.mainText.text = activeAbility.humanReadableName
         containerView.subText.text = activeAbility.description
-        val validUntil = activeAbility.validUntil
-        if (validUntil != null) {
-            containerView.time.text =
-                "Закончится " + PrettyTime(Locale("ru")).format(Date(validUntil))
-        }
 
         containerView.setOnClickListener {
             onClick?.invoke()
         }
+
+        disposer += Observable.interval(0, 10, TimeUnit.SECONDS)
+            .observeOn(MainThreadSchedulers.androidUiScheduler)
+            .subscribe {
+                val currentTimestamp = System.currentTimeMillis()
+                val validUntil = activeAbility.validUntil
+                if (validUntil != null) {
+                    containerView.time.text =
+                        "Закончится " + PrettyTime(Locale("ru")).format(Date(validUntil))
+                } else if (activeAbility.cooldownUntil >= currentTimestamp) {
+                    containerView.time.text =
+                        "Доступно " + PrettyTime(Locale("ru")).format(Date(activeAbility.cooldownUntil))
+                } else {
+                    containerView.time.text = ""
+                }
+            }
     }
 }
--- features/view/universal_list/src/main/java/org/shadowrunrussia2020/android/view/universal_list/PassiveAbilityListItem.kt
+++ features/view/universal_list/src/main/java/org/shadowrunrussia2020/android/view/universal_list/PassiveAbilityListItem.kt
@@ -4,11 +4,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.generic_recycler_view_item.view.*
 import org.ocpsoft.prettytime.PrettyTime
 import org.shadowrunrussia2020.android.common.models.PassiveAbility
+import org.shadowrunrussia2020.android.common.utils.MainThreadSchedulers
+import org.shadowrunrussia2020.android.common.utils.plusAssign
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 fun createPassiveAbilityHeader(list:Collection<UniversalViewData>) = HeaderItem(R.id.passive_ability_item, "Пассивные способности", R.drawable.power, list)
 
@@ -23,20 +28,31 @@ class PassiveAbilityListItem(
 }
 
 private class PassiveAbilityViewHolder private constructor(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    private val disposer = CompositeDisposable()
+
     constructor(parent: ViewGroup)
             : this(LayoutInflater.from(parent.context).inflate(R.layout.generic_recycler_view_item, parent, false))
 
     fun bindView(passiveAbility: PassiveAbility, onClick: (() -> Unit)?, hide: Boolean) {
+        disposer.clear()
+
         containerView.mainText.text = passiveAbility.name
         containerView.subText.text = passiveAbility.description
-        val validUntil = passiveAbility.validUntil
-        if (validUntil != null) {
-            containerView.time.text =
-                "Закончится " + PrettyTime(Locale("ru")).format(Date(validUntil))
-        }
+
 
         containerView.setOnClickListener {
             onClick?.invoke()
         }
+
+        disposer += Observable.interval(0, 10, TimeUnit.SECONDS)
+            .observeOn(MainThreadSchedulers.androidUiScheduler)
+            .subscribe {
+                val currentTimestamp = System.currentTimeMillis()
+                val validUntil = passiveAbility.validUntil
+                if (validUntil != null) {
+                    containerView.time.text =
+                        "Закончится " + PrettyTime(Locale("ru")).format(Date(validUntil))
+                }
+            }
     }
 }
