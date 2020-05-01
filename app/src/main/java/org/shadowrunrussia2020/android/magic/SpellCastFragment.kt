d675e76ae9564ee6ef7847713758177279169653
Alexey Eremin <a.eremin.msu@gmail.com>
2020-05-01 20:39:14 +0200

    Support reagents and ritual members on client

--- app/src/main/java/org/shadowrunrussia2020/android/magic/SpellCastFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/magic/SpellCastFragment.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -16,11 +18,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.shadowrunrussia2020.android.R
 import org.shadowrunrussia2020.android.character.CharacterViewModel
-import org.shadowrunrussia2020.android.common.models.HistoryRecord
-import org.shadowrunrussia2020.android.common.models.Spell
-import org.shadowrunrussia2020.android.common.utils.showErrorMessage
 import org.shadowrunrussia2020.android.common.models.FullQrData
+import org.shadowrunrussia2020.android.common.models.HistoryRecord
 import org.shadowrunrussia2020.android.common.models.QrType
+import org.shadowrunrussia2020.android.common.models.Spell
+import org.shadowrunrussia2020.android.common.utils.russianQrType
+import org.shadowrunrussia2020.android.common.utils.showErrorMessage
 import org.shadowrunrussia2020.android.model.qr.maybeQrScanned
 import org.shadowrunrussia2020.android.model.qr.startQrScan
 
@@ -54,24 +57,85 @@ class SpellCastFragment : Fragment() {
         textAbilityName.text = "${spell.humanReadableName} (мощь ${castModel.power})"
         textAbilityDescription.text = spell.description
 
+        mCharacterModel.getCharacter().observe(this, Observer{character ->
+            if (character != null) {
+                addRitualMember.isVisible = character.passiveAbilities.any { it.id.contains("ritual-magic") }
+            }
+        })
+
+        updateButtonLabels()
+
         castSpell.setOnClickListener {
-            if (spell.hasTarget) chooseTarget() else castOnSelf()
+            if (spell.hasTarget) {
+                castModel.qrReason = QrReason.TARGET
+                startQrScan(this, "Выбор цели заклинания.")
+            } else cast()
+        }
+
+        addReagent.setOnClickListener {
+            castModel.qrReason = QrReason.REAGENT
+            startQrScan(this, "Добавление реагента.")
+        }
+
+        addRitualMember.setOnClickListener {
+            castModel.qrReason = QrReason.RITUAL_MEMBER
+            startQrScan(this, "Добавление участника ритуала.")
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         maybeQrScanned(requireActivity(), requestCode, resultCode, data, {
-            // TODO(aeremin) Figure out what kind of QR we are scanning
-            castOnTarget(it)
+            when (castModel.qrReason) {
+                QrReason.REAGENT -> onAddReagent(it)
+                QrReason.RITUAL_MEMBER -> onAddRitualMember(it)
+                QrReason.TARGET -> onTargetPicked(it)
+                null -> {}
+            }
+            updateButtonLabels()
+            castModel.qrReason = null
         })
     }
 
-    private fun castOnSelf() {
+    private fun onAddReagent(fullQrData: FullQrData) {
+        if (fullQrData.type != QrType.reagent) {
+            showErrorMessage(requireContext(), "Неожиданный QR-код: ${russianQrType(fullQrData.type)}. Подходящие: Реагент.");
+            return
+        }
+
+        castModel.reagentIds.add(fullQrData.modelId)
+    }
+
+    private fun onAddRitualMember(fullQrData: FullQrData) {
+        if (fullQrData.type != QrType.HEALTHY_BODY && fullQrData.type != QrType.WOUNDED_BODY) {
+            showErrorMessage(requireContext(), "Неожиданный QR-код: ${russianQrType(fullQrData.type)}. Подходящие: Живое физическое тело.");
+            return
+        }
+
+        castModel.ritualMembersIds.add(fullQrData.modelId)
+    }
+
+    private fun onTargetPicked(fullQrData: FullQrData) {
+        if (fullQrData.type != QrType.HEALTHY_BODY && fullQrData.type != QrType.WOUNDED_BODY) {
+            showErrorMessage(requireContext(), "Неожиданный QR-код: ${russianQrType(fullQrData.type)}. Подходящие: Живое физическое тело.");
+            return
+        }
+
+        castModel.targetCharacterId = fullQrData.modelId
+        cast()
+    }
+
+    private fun cast() {
         CoroutineScope(Dispatchers.Main).launch {
             try {
-                val response = mCharacterModel.castSpell(
-                    castModel.id, hashMapOf("power" to castModel.power)
+                val data: HashMap<String, Any> = hashMapOf(
+                    "power" to castModel.power,
+                    "ritualMembersIds" to castModel.ritualMembersIds.toList(),
+                    "reagentIds" to castModel.reagentIds.toList()
                 )
+                if (castModel.targetCharacterId != null) {
+                    data["targetCharacterId"] = castModel.targetCharacterId!!
+                }
+                val response = mCharacterModel.castSpell(castModel.id, data)
 
                 response?.tableResponse?.let { tableResponse ->
                     findNavController().navigate(
@@ -86,7 +150,6 @@ class SpellCastFragment : Fragment() {
                         )
                     )
                 } ?: goBackToSpellbook()
-
             } catch (e: Exception) {
                 showErrorMessage(requireContext(), "Ошибка. ${e.message}")
                 goBackToSpellbook()
@@ -94,31 +157,12 @@ class SpellCastFragment : Fragment() {
         }
     }
 
-    private fun chooseTarget() {
-        startQrScan(this, "Выбор цели заклинания.")
-    }
-
-    private fun castOnTarget(qrData: FullQrData) {
-        val eventData = when (qrData.type) {
-            QrType.HEALTHY_BODY, QrType.WOUNDED_BODY -> hashMapOf<String, Any>(
-                "targetCharacterId" to qrData.modelId,
-                "power" to castModel.power
-            )
-            else -> {
-                showErrorMessage(requireContext(), "Ошибка. Неожиданный QR-код."); return
-            }
-        }
-        CoroutineScope(Dispatchers.Main).launch {
-            try {
-                mCharacterModel.castSpell(castModel.id, eventData)
-            } catch (e: Exception) {
-                showErrorMessage(requireContext(), "Ошибка. ${e.message}")
-            }
-            goBackToSpellbook()
-        }
-    }
-
     private fun goBackToSpellbook() {
         findNavController().popBackStack(R.id.spellbookFragment, false)
+    }
+
+    private fun updateButtonLabels() {
+        addReagent.text = "Реагент (${castModel.reagentIds.size})"
+        addRitualMember.text = "Участник ритуала (${castModel.ritualMembersIds.size})"
     }
 }
--- app/src/main/java/org/shadowrunrussia2020/android/magic/SpellCastViewModel.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/magic/SpellCastViewModel.kt
@@ -3,12 +3,19 @@ package org.shadowrunrussia2020.android.magic
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
+enum class QrReason {
+    REAGENT,
+    RITUAL_MEMBER,
+    TARGET,
+}
+
 class SpellCastViewModel(application: Application) : AndroidViewModel(application) {
     var secondsLeft = 0
     var id = ""
     var power = 0
-    var reagentIds = listOf<String>()
-    var ritualMemberIds = listOf<String>()
+    var reagentIds = mutableSetOf<String>()
+    var ritualMembersIds = mutableSetOf<String>()
     var focusId: String? = null
     var targetCharacterId: String? = null
+    var qrReason: QrReason? = null
 }
