07efa5738c8dc81fd514152089af67f6871cc3cf
Alexey Eremin <a.eremin.msu@gmail.com>
2020-05-16 13:35:25 +0200

    Show relative time in the details of the history entry

--- app/src/main/java/org/shadowrunrussia2020/android/HistoryRecordDetailsFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/HistoryRecordDetailsFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_history_record_details.*
+import org.ocpsoft.prettytime.PrettyTime
 import org.shadowrunrussia2020.android.common.models.HistoryRecord
+import java.util.*
 
 class HistoryRecordDetailsFragment : Fragment() {
     private val args: HistoryRecordDetailsFragmentArgs by navArgs()
@@ -25,6 +27,7 @@ class HistoryRecordDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         textHistoryRecordTitle.text = record.title
+        textHistoryRecordTime.text = PrettyTime(Locale("ru")).format(Date(record.timestamp))
         textHistoryRecordText.text =
             if (record.longText.isNotEmpty()) record.longText else record.shortText
     }
