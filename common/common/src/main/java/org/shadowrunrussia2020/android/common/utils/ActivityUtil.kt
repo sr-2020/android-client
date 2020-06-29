18e6fdb3deedcdd4abc19430bab24530e7a4a8e3
Alexey Eremin <a.eremin.msu@gmail.com>
2020-06-29 23:33:14 +0200

    Remove unused variables

--- common/common/src/main/java/org/shadowrunrussia2020/android/common/utils/ActivityUtil.kt
+++ common/common/src/main/java/org/shadowrunrussia2020/android/common/utils/ActivityUtil.kt
@@ -6,20 +6,20 @@ import android.widget.Toast
 import es.dmoral.toasty.Toasty
 
 fun showErrorMessage(context: Context, msg: String) {
-    val t = Toasty.error(context, msg, Toast.LENGTH_LONG, true).apply {
+    Toasty.error(context, msg, Toast.LENGTH_LONG, true).apply {
         setGravity(Gravity.CENTER, 0, 0)
         show()
     }
 }
 
 fun showInfoMessage(context: Context, msg: String) {
-    val t = Toasty.info(context, msg, Toast.LENGTH_LONG, true).apply {
+    Toasty.info(context, msg, Toast.LENGTH_LONG, true).apply {
         show()
     }
 }
 
 fun showSuccessMessage(context: Context, msg: String) {
-    val t = Toasty.success(context, msg, Toast.LENGTH_SHORT, true).apply {
+    Toasty.success(context, msg, Toast.LENGTH_SHORT, true).apply {
         show()
     }
 }
