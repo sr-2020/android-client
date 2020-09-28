ce07e08bf7aaf21c8e9726189e2ee234d8e8548d
Alexey Eremin <a.eremin.msu@gmail.com>
2020-09-28 20:33:50 +0200

    Use universal list adapter on billing history screen

--- app/src/main/java/org/shadowrunrussia2020/android/billing/BillingHistoryFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/billing/BillingHistoryFragment.kt
@@ -7,14 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_billing_history.*
 import org.shadowrunrussia2020.android.R
 import org.shadowrunrussia2020.android.common.models.Transaction
+import org.shadowrunrussia2020.android.view.universal_list.TransactionListItem
+import org.shadowrunrussia2020.android.view.universal_list.UniversalAdapter
 
 class BillingHistoryFragment : Fragment() {
 
-    private lateinit var mModel: BillingViewModel
+    private val mModel by lazy {
+        ViewModelProviders.of(activity!!).get(BillingViewModel::class.java)
+    }
+    private val universalAdapter by lazy { UniversalAdapter() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_billing_history, container, false)
@@ -23,18 +29,28 @@ class BillingHistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mModel = ViewModelProviders.of(activity!!).get(BillingViewModel::class.java)
-
-        // See RecyclerView guide for details if needed
-        // https://developer.android.com/guide/topics/ui/layout/recyclerview
-        transactionHistoryView.setHasFixedSize(true)
-        transactionHistoryView.layoutManager = LinearLayoutManager(activity!!)
-
-        val adapter = TransactionsAdapter()
         mModel.getHistory().observe(this,
-            Observer { data: List<Transaction>? -> if (data != null) adapter.setData(data) })
+            Observer { transactions: List<Transaction>? ->
+                if (transactions != null) {
+                    universalAdapter.clear()
 
-        transactionHistoryView.adapter = adapter
+                    universalAdapter.appendList(
+                        transactions.map {
+                            TransactionListItem(it) {
+                                findNavController().navigate(
+                                    BillingFragmentDirections.actionTransactionDetails(
+                                        it
+                                    )
+                                )
+                            }
+                        })
+
+                    universalAdapter.apply()
+                }
+            })
+
+        transactionHistoryView.adapter = universalAdapter
+        transactionHistoryView.layoutManager = LinearLayoutManager(requireContext())
     }
 }
 
---
+++ features/view/universal_list/src/main/java/org/shadowrunrussia2020/android/view/universal_list/TransactionListItem.kt
@@ -0,0 +1,50 @@
+package org.shadowrunrussia2020.android.view.universal_list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.transaction_item.view.*
+import org.ocpsoft.prettytime.PrettyTime
+import org.shadowrunrussia2020.android.common.models.Transaction
+import java.util.*
+
+class TransactionListItem(
+    private val transaction: Transaction,
+    private val onClick: (() -> Unit)? = null
+) : UniversalViewData() {
+    override val groupID = R.id.rent_item
+    override val isHeader = false
+    override fun createViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = TransactionViewHolder(parent)
+    override fun bindHolder(holder: RecyclerView.ViewHolder) = (holder as TransactionViewHolder).bindView(transaction, onClick, hide)
+}
+
+private class TransactionViewHolder private constructor(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    constructor(parent: ViewGroup)
+            : this(LayoutInflater.from(parent.context).inflate(R.layout.transaction_item, parent, false))
+
+    fun bindView(transaction: Transaction, onClick: (() -> Unit)?, hide: Boolean) {
+
+        if (transaction.transferType == "Incoming") {
+            containerView.direction.setImageResource(R.drawable.ic_transfer_to)
+            containerView.comment.text = commentInBrackets(transaction)
+            containerView.amount.text = transaction.amount.toString()
+        } else {
+            containerView.direction.setImageResource(R.drawable.ic_transfer_from)
+            containerView.comment.text = commentInBrackets(transaction)
+            containerView.amount.text = (-transaction.amount).toString()
+        }
+        containerView.time.text  = PrettyTime(Locale("ru")).format(transaction.operationTime)
+
+        containerView.setOnClickListener {
+            onClick?.invoke()
+        }
+    }
+
+    private fun commentInBrackets(t: Transaction): String {
+        val comment = t.comment
+        if (comment == null || comment.isEmpty()) return ""
+        return "($comment)"
+    }
+}
