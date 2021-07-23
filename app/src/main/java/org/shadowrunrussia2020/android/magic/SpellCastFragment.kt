7b1504731b7b16b16a476521187563f170ed2895
Alexander Chuprin <murmail@gmail.com>
2021-07-23 13:47:46 +0300

    IMPL: show participants and victims in spell results
    Trackpoint, Trackball don't show participants and victims, but should do it

--- app/src/main/java/org/shadowrunrussia2020/android/magic/SpellCastFragment.kt
+++ app/src/main/java/org/shadowrunrussia2020/android/magic/SpellCastFragment.kt
@@ -236,7 +236,13 @@ class SpellCastFragment : Fragment() {
                                 HistoryRecord(
                                     "", it.timestamp,
                                     "${it.spellName}, мощь: ${it.power}, откат: ${it.magicFeedback}",
-                                    it.casterAura, ""
+                                    it.casterAura,
+                                    "" + (it.participantsAmount != null and it.participantsAmount > 0
+                                          ? ", участники: ${it.participantsAmount}"
+                                          :"")
+                                       + (it.victimsAmount != null and it.victimsAmount > 0
+                                          ? ", жертвы: ${it.victimsAmount}"
+                                          : "")
                                 )
                             }.toTypedArray()
                         )
